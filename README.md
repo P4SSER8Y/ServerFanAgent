30f253343aace6fbec7c66eebcc043d82c7f88b7
P4SSER8Y <p4sser8y@outlook.com>
2021-05-30 10:28:39 +0800

    📦 NEW: add report_running

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ ServerFan Agent
 Changelogs
 ----------------
 + 2021-05-30
+    - v0.4
+        - add API of report_running
     - v0.3
         - handle exceptions and print callstack
     - v0.2
--- ServerFan.py
+++ ServerFan.py
@@ -14,6 +14,8 @@ logger = logging.getLogger("ServerFan")
 API_GET_JOB = "/agent/job"
 API_AUTO_MESSAGE = "/agent/message/auto"
 API_REPORT_SUCCESS = "/agent/jobs/{id}/succeed"
+API_REPORT_FAIL = "/agent/jobs/{id}/fail"
+API_REPORT_RUNNING = "/agent/jobs/{id}/running"
 HEADER = {
     "Authorization": f"Bearer {secret.token}",
 }
@@ -37,7 +39,36 @@ def report_succeed(id=None, result=None):
     if ret.ok:
         logger.debug(f"send succeed message done with ret_code={ret.status_code}")
     else:
+        logger.debug(f"content: {ret.content}")
         logger.error(f"send succeed message failed with ret_code={ret.status_code}")
+
+
+def report_fail(id=None, result=None):
+    if id is None:
+        logger.error("please provide id")
+        return
+    data = {"result": result}
+    url = urljoin(secret.url, API_REPORT_FAIL.format(id=id))
+    ret = put(url=url, headers=HEADER, data=data)
+    if ret.ok:
+        logger.debug(f"send fail message done with ret_code={ret.status_code}")
+    else:
+        logger.debug(f"content: {ret.content}")
+        logger.error(f"send fail message failed with ret_code={ret.status_code}")
+
+
+def report_running(id=None, result=None):
+    if id is None:
+        logger.error("please provide id")
+        return
+    data = {"result": result}
+    url = urljoin(secret.url, API_REPORT_RUNNING.format(id=id))
+    ret = put(url=url, headers=HEADER, data=data)
+    if ret.ok:
+        logger.debug(f"send running message done with ret_code={ret.status_code}")
+    else:
+        logger.debug(f"content: {ret.content}")
+        logger.error(f"send running message failed with ret_code={ret.status_code}")
 
 
 def send_auto_message(message=None):
@@ -47,4 +78,5 @@ def send_auto_message(message=None):
     if ret.ok:
         logger.debug(f"send auto message done with ret_code={ret.status_code}")
     else:
+        logger.debug(f"content: {ret.content}")
         logger.error(f"send auto message failed with ret_code={ret.status_code}")
